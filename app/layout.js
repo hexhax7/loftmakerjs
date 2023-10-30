581a7e722ab73a485baf2a86e8bd6b5894844d38
hexhax7 <87332576+hexhax7@users.noreply.github.com>
2023-10-30 17:46:25 +0000

    google ads

--- app/layout.js
+++ app/layout.js
@@ -3,7 +3,7 @@ import Head from 'next/head';
 import './globals.css'
 import Navbar from './navbar/page'
 import { Analytics } from '@vercel/analytics/react';
-
+import Script  from 'next/script';
 export const metadata = {
   title: 'Loft Makers London Ltd',
   description: 'Loft Makers London Ltd',
@@ -13,15 +13,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-DP4TSF0JWV">
-      </script>
-      <script>
+      <Script async src="https://www.googletagmanager.com/gtag/js?id=G-DP4TSF0JWV">
+      </Script>
+      <Script>
         window.dataLayer = window.dataLayer || [];
         function gtag(){dataLayer.push(arguments)}
         gtag('js', new Date());
 
         gtag('config', 'G-DP4TSF0JWV');
-      </script>
+      </Script>
       </Head>
       <body>
         <Navbar/>
